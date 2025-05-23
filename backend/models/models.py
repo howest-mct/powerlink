e26f2e584e2c0bc870f5fc35ca9d3d813f1713a4
Michiel-Gekiere <michiel.gekiere@student.howest.be>
2025-05-23 13:41:29 +0200

    Add DTO and repository methods for lamp status management

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
-
-
-class foo(BaseModel):
-    pass
+ 
+ 
+class DTOLampStatus(BaseModel):
+    nieuwe_status: int
+ 
+ 
+class LampStatus(BaseModel):
+    lamp: int
+    status: int
