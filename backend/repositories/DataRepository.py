2378cc4b9dcc20035e8f04ab2354eced4e470cc8
Michiel Gekiere <michiel.gekiere@student.howest.be>
2025-05-24 14:15:59 +0200

    Remove individual room HTML files and styles, consolidating into a single index.html for the home interface.

--- backend/repositories/DataRepository.py
+++ backend/repositories/DataRepository.py
@@ -1,26 +1,5 @@
 from .Database import Database
- 
- 
+
+
 class DataRepository:
-    @staticmethod
-    def read_status_lampen():
-        sql = "SELECT * from lampen"
-        return Database.get_rows(sql)
- 
-    @staticmethod
-    def read_status_lamp_by_id(id):
-        sql = "SELECT * from lampen WHERE id = %s"
-        params = [id]
-        return Database.get_one_row(sql, params)
- 
-    @staticmethod
-    def update_status_lamp(id, status):
-        sql = "UPDATE lampen SET status = %s WHERE id = %s"
-        params = [status, id]
-        return Database.execute_sql(sql, params)
- 
-    @staticmethod
-    def update_status_alle_lampen(status):
-        sql = "UPDATE lampen SET status = %s"
-        params = [status]
-        return Database.execute_sql(sql, params)
+    pass
